d290999f7a5c19ac4cc45b406882e906b3163f35
Dingchao Zhang <dingchao@stanford.edu>
2019-10-24 22:55:54 -0500

    Update raspi_fire.py

--- raspi_fire.py
+++ raspi_fire.py
@@ -89,66 +89,72 @@ def pothole_detect(frame):
     # dim = (width, height)
     # # resize image
     # resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    frame = f.array
-    copy = frame.copy()
-    blur = cv2.GaussianBlur(frame, (21, 21), 0)
-    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+with picamera.PiCamera() as camera:
+    stream = io.BytesIO()
+    for f in camera.capture_continuous(stream, format="bgr", use_video_port=True):
+        stream.truncate()
+        stream.seek(0)
+        if process(stream):
+            break
+        frame = f.array
+        copy = frame.copy()
+        blur = cv2.GaussianBlur(frame, (21, 21), 0)
+        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-    # For HSV, Hue range is [0,179], Saturation range is [0,255] and
-    # Value range is [0,255]
-    # lower = np.array([18, 90, 90], dtype="uint8")
-    # upper = np.array([35, 255, 255], dtype="uint8")
-    # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv
-    lower = np.array([0, 155, 20], dtype="uint8")
-    upper = np.array([20, 255, 255], dtype="uint8")
-    mask = cv2.inRange(hsv, lower, upper)
+        # For HSV, Hue range is [0,179], Saturation range is [0,255] and
+        # Value range is [0,255]
+        # lower = np.array([18, 90, 90], dtype="uint8")
+        # upper = np.array([35, 255, 255], dtype="uint8")
+        # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv
+        lower = np.array([0, 155, 20], dtype="uint8")
+        upper = np.array([20, 255, 255], dtype="uint8")
+        mask = cv2.inRange(hsv, lower, upper)
 
-    # here insert the bgr values which you want to convert to hsv
-    # red = np.uint8([[[0, 0, 255]]])
-    # hsvRed = cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
-    #
-    # lowerLimit = hsvRed[0][0][0] - 10, 100, 100
-    # upperLimit = hsvRed[0][0][0] + 10, 255, 255
+        # here insert the bgr values which you want to convert to hsv
+        # red = np.uint8([[[0, 0, 255]]])
+        # hsvRed = cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
+        #
+        # lowerLimit = hsvRed[0][0][0] - 10, 100, 100
+        # upperLimit = hsvRed[0][0][0] + 10, 255, 255
 
-    # lower = np.array([177, 205, 20], dtype="uint8")
-    # upper = np.array([179, 255, 255], dtype="uint8")
-    # mask2 = cv2.inRange(hsv, lower, upper)
-    #
-    # mask = cv2.bitwise_or(mask1, mask2)
-    # mask = mask1
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-    # omask = cv2.morphologyEx(1 - mask, cv2.MORPH_OPEN, kernel)
+        # lower = np.array([177, 205, 20], dtype="uint8")
+        # upper = np.array([179, 255, 255], dtype="uint8")
+        # mask2 = cv2.inRange(hsv, lower, upper)
+        #
+        # mask = cv2.bitwise_or(mask1, mask2)
+        # mask = mask1
+        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+        # omask = cv2.morphologyEx(1 - mask, cv2.MORPH_OPEN, kernel)
 
-    contours, hierarchy = cv2.findContours(
-        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        _ , contours, hierarchy = cv2.findContours(
+            mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    output = cv2.bitwise_and(frame, hsv, mask=mask)
-    no_red = cv2.countNonZero(mask)
+        output = cv2.bitwise_and(frame, hsv, mask=mask)
+        no_red = cv2.countNonZero(mask)
 
-    if int(no_red) > 30000:
-        print('Fire detected')
-        if len(contours) > 0:
-            print('countours detected:', len(contours))
-            for contour in contours:
-                x, y, w, h = cv2.boundingRect(contour)
-                cv2.drawContours(
-                    copy, [contour], -1, (0, 128, 128), 3)
-                cv2.rectangle(copy, (x, y), (x + w, y + h), (124, 252, 0), 2)
-        cv2.putText(copy, 'Fire Detected!',
-                    bottomLeftCornerOfText,
-                    font,
-                    fontScale,
-                    fontColor,
-                    lineType)
-        # send_info()
-        # playsound(VOICE_FILE)
+        if int(no_red) > 30000:
+            print('Fire detected')
+            if len(contours) > 0:
+                print('countours detected:', len(contours))
+                for contour in contours:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    cv2.drawContours(
+                        copy, [contour], -1, (0, 128, 128), 3)
+                    cv2.rectangle(copy, (x, y), (x + w, y + h), (124, 252, 0), 2)
+            cv2.putText(copy, 'Fire Detected!',
+                        bottomLeftCornerOfText,
+                        font,
+                        fontScale,
+                        fontColor,
+                        lineType)
+            # send_info()
+            # playsound(VOICE_FILE)
 
-    print(int(no_red))
-    cv2.imshow("output", copy)
+        print(int(no_red))
+        cv2.imshow("output", copy)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cv2.destroyAllWindows()
 video.release()
